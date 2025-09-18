5474e9d4734539d486112aa8a65bf6b9f90b9e95
unknown <linkinmjs@gmail.com>
2025-09-17 21:16:00 -0300

    Add shoot effect

--- godot/scripts/top_scene.gd
+++ godot/scripts/top_scene.gd
@@ -3,6 +3,8 @@ extends Node2D
 @onready var cannon_position: Marker2D = $Ship/CannonPosition
 @onready var cannon_ball_scene = preload("res://scenes/cannon_ball.tscn")
 @onready var shoot_delay: Timer = $ShootDelay
+@onready var shoot_animation: CPUParticles2D = $Ship/CannonPosition/ShootAnimation
+
 
 func _process(delta: float) -> void:
 	# TODO replace next hardcode
@@ -14,4 +16,5 @@ func shoot() -> void:
 	await shoot_delay.timeout
 	var cannon_ball: RigidBody2D = cannon_ball_scene.instantiate()
 	cannon_ball.global_position = cannon_position.global_position
+	shoot_animation.restart()
 	add_child(cannon_ball)
