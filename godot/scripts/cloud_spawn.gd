49433b5de6d86a8bc0652b0c39cca8a35c4f52da
unknown <linkinmjs@gmail.com>
2025-09-25 22:11:38 -0300

    Add bottom clouds

--- godot/scripts/cloud_spawn.gd
+++ godot/scripts/cloud_spawn.gd
@@ -2,9 +2,21 @@ extends Path2D
 
 @onready var spawn_timer: Timer = $SpawnTimer
 @export var clouds_scenes_to_spawn: Array[PackedScene]
+@export var timer_wait_time: float = 0.8
+@export_enum("Top", "Bottom") var spawn_on_scene: String
+
+var is_active = true
 
 func _ready() -> void:
 	spawn_timer.timeout.connect(_on_spawn_timer_timeout)
+	spawn_timer.wait_time = timer_wait_time
+
+#TODO: improve next code. Convert this _process to signals
+func _process(delta: float) -> void:
+	if not is_active:
+		spawn_timer.paused = true
+	elif is_active:
+		spawn_timer.paused = false
 
 func _on_spawn_timer_timeout() -> void:
 	var selected_scene_packed: PackedScene = clouds_scenes_to_spawn.pick_random()
